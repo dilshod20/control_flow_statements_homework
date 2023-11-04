3329b750c70de1a1547d92aad6b15ba2969541b1
dilshod20 <dilshodraxmatov277@gmail.com>
2023-11-04 15:23:47 +0500

    3

--- if03.py
+++ if03.py
@@ -7,4 +7,11 @@ def main(a):
     Returns:
         a: integer
     """
-    return
+    if a > 0:
+        a = a + 1
+    if a < 0:
+        a = a - 2
+    return a
+
+print(main(-9))
+print(main(3))         
